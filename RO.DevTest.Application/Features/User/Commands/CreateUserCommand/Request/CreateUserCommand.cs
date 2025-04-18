7411b49ee58e828ab6af007b9dd360ef46d4ac92
egotting <henricodss@gmail.com>
2025-04-18 12:55:01 -0300

    Troquei os responses para em vez de retornar os valores retornar o status

--- RO.DevTest.Application/Features/User/Commands/CreateUserCommand/Request/CreateUserCommand.cs
+++ RO.DevTest.Application/Features/User/Commands/CreateUserCommand/Request/CreateUserCommand.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Response.General;
 using RO.DevTest.Domain.Enums;
 
-namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand;
+namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Request;
 
-public class CreateUserCommand : IRequest<CreateUserResult> {
+public class CreateUserCommand : IRequest<GeneralResult<ResponseStatus.ResponseStatus>>
+{
     public string UserName { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
@@ -11,8 +13,10 @@ public class CreateUserCommand : IRequest<CreateUserResult> {
     public string PasswordConfirmation { get; set; } = string.Empty;
     public UserRoles Role { get; set; }
 
-    public Domain.Entities.User AssignTo() {
-        return new Domain.Entities.User {
+    public Domain.Entities.User AssignTo()
+    {
+        return new Domain.Entities.User
+        {
             UserName = UserName,
             Email = Email,
             Name = Name,
