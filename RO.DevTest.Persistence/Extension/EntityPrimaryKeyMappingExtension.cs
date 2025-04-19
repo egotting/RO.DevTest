e982e244d3a488ca5142f998b2c97cf9fa8986cf
egotting <henricodss@gmail.com>
2025-04-19 14:32:05 -0300

    Implementaçoes para resolver erro ao criar migrations

---
+++ RO.DevTest.Persistence/Extension/EntityPrimaryKeyMappingExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RO.DevTest.Domain.Entities;
+
+namespace RO.DevTest.Persistence.Extension;
+
+public static class EntityPrimaryKeyMappingExtension
+{
+    public static ModelBuilder ConfigurePrimaryKey(this ModelBuilder mb)
+    {
+        mb.Entity<User>().HasKey(x => x.Id);
+        return mb;
+    }
+}
---
+++ RO.DevTest.Persistence/Extension/MigrationExtension.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace RO.DevTest.Persistence.Extension;
+
+public static class MigrationExtension
+{
+    public async static Task RunMigration(this IServiceProvider service)
+    {
+        using (var scope = service.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var maxRetryAttempts = 2;
+            var delayBetweenRetries = TimeSpan.FromSeconds(2);
+            for (var attempt = 0; attempt < maxRetryAttempts; attempt++)
+            {
+                try
+                {
+                    var dbContext = services.GetRequiredService<DefaultContext>();
+                    var pedingMigrations = dbContext.Database.GetPendingMigrations();
+                    if (pedingMigrations.Any())
+                    {
+                        Console.WriteLine("Have peding migrations: Apply migrations...");
+                        dbContext.Database.Migrate();
+                    }
+
+                    Console.WriteLine("Dont have any pending migrations...");
+                    break;
+                }
+                catch (PostgresException e)
+                {
+                    if (e.SqlState == "42P07")
+                    {
+                        Console.WriteLine("Error: This migration already exist, Aborting migration.");
+                        break;
+                    }
+
+                    Console.WriteLine($"Error when try to verify/apply migration (try  {attempt + 1}):  {e.Message}");
+                    if (attempt < maxRetryAttempts - 1)
+                    {
+                        Console.WriteLine($"Waiting.. {delayBetweenRetries / 1000} Seconds before to try again...");
+                        await Task.Delay(delayBetweenRetries);
+                    }
+                }
+            }
+        }
+    }
+}
