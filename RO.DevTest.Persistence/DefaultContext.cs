af6673c3c2a4d1d3acec229963963b1eea06f0df
egotting <henricodss@gmail.com>
2025-04-19 14:34:36 -0300

    Implementei o DbSet e tambem implementei configuraçao para checar se tem o Id

--- RO.DevTest.Persistence/DefaultContext.cs
+++ RO.DevTest.Persistence/DefaultContext.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RO.DevTest.Domain.Entities;
+using RO.DevTest.Persistence.Extension;
 
 namespace RO.DevTest.Persistence;
 
-public class DefaultContext : IdentityDbContext<User> {
+public class DefaultContext : IdentityDbContext<User>
+{
 
-    public DefaultContext() { }
+    public DbSet<User> User { get; set; }
+    public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
+    {
+    }
 
-    public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder builder) {
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
         builder.HasPostgresExtension("uuid-ossp");
         builder.ApplyConfigurationsFromAssembly(typeof(DefaultContext).Assembly);
-
+        builder.ConfigurePrimaryKey();
+                
         base.OnModelCreating(builder);
     }
 }
