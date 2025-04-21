c4ae458f12ad7df7ea75e0e8280764f68ddff6c5
egotting <henricodss@gmail.com>
2025-04-21 17:17:25 -0300

    Criando response para a listagem de usuarios

---
+++ RO.DevTest.Application/Features/User/Commands/CreateUserCommand/Response/GetUsersResult.cs
@@ -0,0 +1,20 @@
+﻿namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Response;
+
+public class GetUsersResult
+{
+    public GetUsersResult()
+    {
+    }
+
+    public GetUsersResult(Domain.Entities.User user)
+    {
+        UserName = user.UserName!;
+        Email = user.Email!;
+        Name = user.Name;
+    }
+
+
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+}
