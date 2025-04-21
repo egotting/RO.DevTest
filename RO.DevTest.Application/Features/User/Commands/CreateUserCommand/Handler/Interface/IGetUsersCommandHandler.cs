b9d7a07080940a15555ce88b91290c1d10106b67
egotting <henricodss@gmail.com>
2025-04-21 17:17:52 -0300

    Criando interface para o handler da listagem de todos usuarios

---
+++ RO.DevTest.Application/Features/User/Commands/CreateUserCommand/Handler/Interface/IGetUsersCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Request;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Response;
+
+namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Handler.Interface;
+
+public interface IGetUsersCommandHandler
+{
+    public Task<IEnumerable<GetUsersResult>> Handle();
+}
