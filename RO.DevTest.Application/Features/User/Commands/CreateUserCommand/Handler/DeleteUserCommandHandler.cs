6f07a7bb9faf5c48ac8fd28c7c8aff4908f302d9
egotting <henricodss@gmail.com>
2025-04-21 17:16:56 -0300

    Implementando novos Handlers

---
+++ RO.DevTest.Application/Features/User/Commands/CreateUserCommand/Handler/DeleteUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using RO.DevTest.Application.Contracts.Infrastructure;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Request;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Validator;
+using RO.DevTest.Application.ResultPattern;
+
+namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Handler.Interface;
+
+public class DeleteUserCommandHandler(IIdentityAbstractor identityAbstractor) : IDeleteUserCommandHandler
+{
+    private readonly IIdentityAbstractor _identityAbstractor = identityAbstractor;
+
+
+    public async Task<Result<Error>> Handle(DeleteUserCommand request,
+        CancellationToken cancellationToken)
+    {
+        DeleteUserCommandValidator validator = new();
+        ValidationResult validationResult = await validator.ValidateAsync(request, cancellationToken);
+        if (!validationResult.IsValid) return Result<Error>.Failure(Error.Validation("User.Not.Valid", "User invalid."));
+
+        var findUser = await _identityAbstractor.FindUserByIdAsync(request.Email);
+
+        if (!findUser.EmailConfirmed) return Result<Error>.Failure(Error.NotFound("User.NotFound", "Not found user email."));
+
+        await _identityAbstractor.DeleteUser(findUser);
+
+        return Result<Error>.Sucess(Error.Success("User.Deleted", "A user account was deleted."));
+    }
+
+}
---
+++ RO.DevTest.Application/Features/User/Commands/CreateUserCommand/Handler/GetUsersCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using RO.DevTest.Application.Contracts.Infrastructure;
+using RO.DevTest.Application.Contracts.Persistance.Repositories;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Handler.Interface;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Request;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Response;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Validator;
+using RO.DevTest.Application.ResultPattern;
+
+namespace RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Handler;
+
+public class GetUsersCommandHandler(IUserRepository userRepository)
+    : IGetUsersCommandHandler
+{
+    private readonly IUserRepository _userRepository = userRepository;
+
+    public async Task<IEnumerable<GetUsersResult>> Handle()
+    {
+
+        var users = await userRepository.GetAll();
+        return users.Select(user => new GetUsersResult
+        {
+            UserName = user.UserName!,
+            Email = user.Email!,
+            Name = user.Name,
+        });
+    }
+}
