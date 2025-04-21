b3864d31c20ba4f01aec07b55e4de91ccf4770cd
egotting <henricodss@gmail.com>
2025-04-21 17:11:59 -0300

    Implementando metodo para retornar todos os usuarios listados de forma ordenada

--- RO.DevTest.Persistence/Repositories/BaseRepository.cs
+++ RO.DevTest.Persistence/Repositories/BaseRepository.cs
@@ -4,29 +4,53 @@ using System.Linq.Expressions;
 
 namespace RO.DevTest.Persistence.Repositories;
 
-public class BaseRepository<T>(DefaultContext defaultContext) : IBaseRepository<T> where T : class {
+public class BaseRepository<T>(DefaultContext defaultContext) : IBaseRepository<T> where T : class
+{
     private readonly DefaultContext _defaultContext = defaultContext;
 
-    protected DefaultContext Context { get => _defaultContext; }
+    protected DefaultContext Context
+    {
+        get => _defaultContext;
+    }
 
-    public async Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default) {
+    public async Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default)
+    {
         await Context.Set<T>().AddAsync(entity, cancellationToken);
         await Context.SaveChangesAsync(cancellationToken);
         return entity;
     }
 
-    public async void Update(T entity) {
+    public async void Update(T entity)
+    {
         Context.Set<T>().Update(entity);
         await Context.SaveChangesAsync();
     }
 
-    public async void Delete(T entity) {
+    public async void Delete(T entity)
+    {
         Context.Set<T>().Remove(entity);
         await Context.SaveChangesAsync();
     }
 
+
+    public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, int? skip = null, int? take = null,
+        params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = Context.Set<T>();
+        if (predicate != null) query = query.Where(predicate);
+
+        foreach (var include in includes) query = query.Include(include);
+
+        if (orderBy != null) query = orderBy(query);
+
+        if (skip.HasValue) query = query.Skip(skip.Value);
+        if (take.HasValue) query = query.Take(take.Value);
+        return query.ToList();
+    }
+
     public T? Get(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
-    => GetQueryWithIncludes(predicate, includes).FirstOrDefault();
+        => GetQueryWithIncludes(predicate, includes).FirstOrDefault();
 
     /// <summary>
     /// Generates a filtered <see cref="IQueryable{T}"/>, based on its
@@ -45,10 +69,12 @@ public class BaseRepository<T>(DefaultContext defaultContext) : IBaseRepository<
     private IQueryable<T> GetQueryWithIncludes(
         Expression<Func<T, bool>> predicate,
         params Expression<Func<T, object>>[] includes
-    ) {
+    )
+    {
         IQueryable<T> baseQuery = GetWhereQuery(predicate);
 
-        foreach(Expression<Func<T, object>> include in includes) {
+        foreach (Expression<Func<T, object>> include in includes)
+        {
             baseQuery = baseQuery.Include(include);
         }
 
@@ -66,14 +92,15 @@ public class BaseRepository<T>(DefaultContext defaultContext) : IBaseRepository<
     /// <returns>S
     /// The <see cref="IQueryable{T}"/>
     /// </returns>
-    private IQueryable<T> GetWhereQuery(Expression<Func<T, bool>> predicate) {
+    private IQueryable<T> GetWhereQuery(Expression<Func<T, bool>> predicate)
+    {
         IQueryable<T> baseQuery = Context.Set<T>();
 
-        if(predicate is not null) {
+        if (predicate is not null)
+        {
             baseQuery = baseQuery.Where(predicate);
         }
 
         return baseQuery;
     }
-
 }
