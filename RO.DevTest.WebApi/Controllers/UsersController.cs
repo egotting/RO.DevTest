95fe54d9c1036a74a9bfa6affef3764613f6e473
egotting <henricodss@gmail.com>
2025-04-21 17:16:32 -0300

    Implementando a Listagem de usuarios e o o handler para deletar usuarios

--- RO.DevTest.WebApi/Controllers/UsersController.cs
+++ RO.DevTest.WebApi/Controllers/UsersController.cs
@@ -1,26 +1,36 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
-using RO.DevTest.Application.Features.User.Commands.CreateUserCommand;
+using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Handler.Interface;
 using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Request;
-using RO.DevTest.Application.Features.User.Commands.CreateUserCommand.Response;
 using RO.DevTest.Application.ResultPattern;
 
 namespace RO.DevTest.WebApi.Controllers;
 
 [Route("api/user")]
 [OpenApiTags("Users")]
-public class UsersController(IMediator mediator) : ControllerBase {
-    private readonly IMediator _mediator = mediator;
-
+public class UsersController(IMediator mediator, IGetUsersCommandHandler handler) : ControllerBase {
+    private readonly IMediator _mediator = mediator;    
+    private readonly IGetUsersCommandHandler _handler = handler;
     
     [HttpPost]
-    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status201Created)]
-    [ProducesResponseType(typeof(CreateUserResult), StatusCodes.Status400BadRequest)]
     public async Task<IResult> CreateUser( CreateUserCommand request) {
         var response = await _mediator.Send(request);
         
         return Results.Extensions.MapResult(response);
+    }
+
+    [HttpDelete]
+    public async Task<IResult> DeleteUser(DeleteUserCommand request)
+    {
+        var response = await _mediator.Send(request);
+        return Results.Extensions.MapResult(response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var response = await _handler.Handle();
+        return Ok(response);
     }
 }
